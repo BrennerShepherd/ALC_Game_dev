b59b3d6b428f8a980e6e6f5404eab23dfe4f3494
Brenner Shepherd <brenner.s985@stu.nebo.edu>
2019-10-11 10:26:39 -0600

    Random spawns for animals

---
+++ Create with code/Prototype 2/Assets/Course Library/Scripts/Destroyoutofbounds.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyoutofbounds : MonoBehaviour
+{
+    public float topBounds = 30;
+    public float lowerBound = -10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBounds)
+        {
+            Destroy(gameObject);
+        }
+        else if(transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
