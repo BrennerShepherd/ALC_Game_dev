55af97b974af8da4c79df19a5b3f8778e9825bcf
Brenner Shepherd <brenner.s985@stu.nebo.edu>
2019-10-23 09:54:22 -0600

    Finished challenge 2

--- Create with code/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Create with code/Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public float fireRate = 0.5f;
+    private float nextFire = 0.0f;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetButton("Jump") && Time.time > nextFire)
+
         {
-            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            nextFire = Time.time + fireRate;
+            GameObject clone = Instantiate(dogPrefab, transform.position, transform.rotation) as GameObject;
+            //Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
         }
     }
 }
