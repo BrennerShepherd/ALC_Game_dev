0c65e20f498d2889ce6f81506b66b9b96d9524c6
Brenner Shepherd <brenner.s985@stu.nebo.edu>
2019-11-06 10:27:38 -0700

    Finished 3.1

---
+++ Create with code/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce;
+    public float gravityModifer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifer;
+    }
+    public bool isOnGround = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&& isOnGround)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
+    }
+}
